67a5ebb0b3b1ba957eb263a709ef716df775e8d3
logantestroete <loganptestroete@gmail.com>
2026-04-29 21:38:41 -0500

    minor bug fix

--- client/src/pages/SurgeryInfoPage.tsx
+++ client/src/pages/SurgeryInfoPage.tsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 
 function SurgeryInfoPage() {
     return (
-        <div className="max-w-[1280px] flex justify-center mx-auto">
+        <div className="max-w-[1280px] flex flex-col justify-center mx-auto">
             <Header />
         </div>
     )
