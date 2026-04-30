5ded1a2256ef55d76fcbea0c8922a42732658821
logantestroete <loganptestroete@gmail.com>
2026-04-30 08:24:19 -0500

    fixed UI issues with the header and different screen sizes

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,15 +3,23 @@ import { Link } from "react-router";
 
 function Header() {
     return (
-        <div className="w-[100%] pt-2 border-b-1">
-            <div className="flex justify-between px-2 items-center border-b-1 pb-2">
-                <div className="text-3xl font-bold">Trayly</div>
-                <div className="">
-                    <button className="mr-3 border-1 px-4 py-2">Login</button>
-                    <button className="border-1 px-4 py-2">Sign up</button>
+        <div className="w-full mx-auto py-2">
+            <div className="flex justify-between px-1 items-center border-b border-gray-300 pb-2">
+                <div className="flex w-[50%] md:w-[75%] items-center h-auto">
+                    <div className="text-3xl font-bold mr-3">Trayly</div>
+                    <nav className="hidden hd:flex justify-around items-center px-2 font-semibold w-[60%] h-[100%]">
+                        <Link to="/">Home</Link>
+                        <Link to="/info">Surgeries</Link>
+                        <Link to="/study">Study</Link>
+                        <Link to="/notes">Notes</Link>
+                    </nav>
+                </div>
+                <div className="w-[50%] hd:w-[25%] flex justify-end px-1">
+                    <button className="border border-gray-300 rounded px-4 py-1 mr-1">Login</button>
+                    <button className="border border-gray-300 rounded px-4 py-1">Sign up</button>
                 </div>
             </div>
-            <nav className="flex justify-between md:justify-around px-4 md:px-[15%] py-2 font-semibold">
+            <nav className="flex justify-between px-4 hd:px-[15%] py-2 font-semibold border-b border-gray-300 hd:hidden">
                 <Link to="/">Home</Link>
                 <Link to="/info">Surgeries</Link>
                 <Link to="/study">Study</Link>
